129f627534ccb053ae0e3c1fca24403164f0799c
Matheus <30qr4xnl@duck.com>
2024-09-25 21:08:04 -0300

    feat/appointment: fazendo uso do gateway de notification usecase schedulePatient.usecase.ts e atualização uts

--- src/adapters/controllers/schedulePatient.controller.ts
+++ src/adapters/controllers/schedulePatient.controller.ts
@@ -2,9 +2,7 @@ import { Controller, Request, Response } from '@/adapters/controllers/controller
 import { ErrorPresenter } from '@/adapters/presenters/error.presenter';
 import { SchedulePresenter } from '@/adapters/presenters/schedule.presenter';
 import { ValidationError } from '@/domain/errors';
-import {
-  SchedulePatientUseCase,
-} from '@/domain/usecases/schedulePatient/schedulePatient.usecase';
+import { SchedulePatientUseCase } from '@/domain/usecases/schedulePatient/schedulePatient.usecase';
 
 export class SchedulePatientController implements Controller {
   constructor(private useCase: SchedulePatientUseCase) { }
